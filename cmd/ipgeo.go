314622583f375800829d85b3edb6423fb89e12cd
Julien Limoges <julienlimoges@gmail.com>
2018-04-02 21:54:53 -0400

    Added several constructs for basic http service

--- cmd/ipgeo.go
+++ cmd/ipgeo.go
@@ -19,39 +19,6 @@ import (
 	"github.com/oklog/run"
 )
 
-// A LocateRequest represents a request to geolocate a given address.
-type LocateRequest struct {
-	Addr net.IP // An IPv4 address
-}
-
-// DecodeLocateRequest inspects an http.Request and extracts the necessary elements
-// into a valid LocateRequest.
-func DecodeLocateRequest(req *http.Request) (*LocateRequest, error) {
-	ip := filepath.Base(req.URL.Path)
-	addr := net.ParseIP(ip)
-	if addr == nil {
-		return nil, fmt.Errorf("invalid request: %s", ip)
-	}
-	addr = addr.To4()
-	if addr == nil {
-		return nil, fmt.Errorf("unsupported ipv6: %s", ip)
-	}
-	return &LocateRequest{Addr: addr}, nil
-}
-
-// A LocateResponse represents a response to a LocateRequest.
-type LocateResponse struct {
-	Country      string `json:"country"`
-	Subdivision1 string `json:"subdivision1"`
-	Subdivision2 string `json:"subdivision2"`
-	City         string `json:"city"`
-}
-
-// An IPGeolocator is a
-type IPGeolocator interface {
-	LocateIP(addr net.IP) (ipgeo.Location, error)
-}
-
 // main launches the program and returns an appropriate error.
 func mainWithError() error {
 	var (
@@ -73,37 +40,9 @@ func mainWithError() error {
 			locsFilepath, netsFilepath, port, verbose)
 	}
 
-	var locator IPGeolocator
-
-	// HTTP handler to respond to geolocation requests.
 	locate := func(w http.ResponseWriter, r *http.Request) {
-		request, err := DecodeLocateRequest(r)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintln(w, err)
-			return
-		}
-
-		location, err := locator.LocateIP(request.Addr)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprintln(w, err)
-			return
-		}
-		response := LocateResponse{
-			Country:      location.Country,
-			Subdivision1: location.Subdivision1,
-			Subdivision2: location.Subdivision2,
-			City:         location.City,
-		}
-		buf, err := json.MarshalIndent(response, "", "\t")
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprintln(w, err)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Write(buf)
+		var locator IPGeolocator
+		handler(locator, w, r)
 	}
 
 	mux := http.NewServeMux()
@@ -170,4 +109,71 @@ func main() {
 	} else {
 		os.Exit(0)
 	}
+}
+
+// An IPGeolocator is a
+type IPGeolocator interface {
+	LocateIP(addr net.IP) (ipgeo.Location, error)
+}
+
+// A LocateRequest represents a request to geolocate a given address.
+type LocateRequest struct {
+	Addr net.IP // An IPv4 address
+}
+
+// DecodeLocateRequest inspects an http.Request and extracts the necessary elements
+// into a valid LocateRequest.
+func DecodeLocateRequest(req *http.Request) (*LocateRequest, error) {
+	ip := filepath.Base(req.URL.Path)
+	addr := net.ParseIP(ip)
+	if addr == nil {
+		return nil, fmt.Errorf("invalid request: %s", ip)
+	}
+	addr = addr.To4()
+	if addr == nil {
+		return nil, fmt.Errorf("unsupported ipv6: %s", ip)
+	}
+	return &LocateRequest{Addr: addr}, nil
+}
+
+// A LocateResponse represents a response to a LocateRequest.
+type LocateResponse struct {
+	Country      string `json:"country"`
+	Subdivision1 string `json:"subdivision1"`
+	Subdivision2 string `json:"subdivision2"`
+	City         string `json:"city"`
+}
+
+func handler(l IPGeolocator, w http.ResponseWriter, r *http.Request) {
+	request, err := DecodeLocateRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	location, err := l.LocateIP(request.Addr)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	resp := LocateResponse{
+		Country:      location.Country,
+		Subdivision1: location.Subdivision1,
+		Subdivision2: location.Subdivision2,
+		City:         location.City,
+	}
+
+	buf, err := json.MarshalIndent(resp, "", "\t")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(buf)
+
 }
--- ipgeo.go
+++ ipgeo.go
@@ -1,5 +1,10 @@
 package ipgeo
 
+import (
+	"fmt"
+	"net"
+)
+
 // A Location represents a geographic region of varying size, from country,
 // to administrative division and subdivision, to city.
 type Location struct {
@@ -7,4 +12,50 @@ type Location struct {
 	Subdivision1 string
 	Subdivision2 string
 	City         string
+}
+
+// LocationID uniquely identifies a single geographic location/region.
+type LocationID int32
+
+// A LocationRepository provides access to a location store.
+type LocationRepository interface {
+	FindByID(id LocationID) (Location, error)
+}
+
+// A NetworkMapper maps an IP address to a known network.
+type NetworkMapper interface {
+	FindNetwork(addr net.IP) (*net.IPNet, error)
+}
+
+// A NetworkLocator maps a known network to a location.
+type NetworkLocator interface {
+	Map(network *net.IPNet) (LocationID, error)
+}
+
+// A Geolocator is a data structure holding the necessary components to allow
+// ip-based geolocation. This Locator operates by first finding a network match
+// for an ip, then mapping the location associated with this network.
+type Geolocator struct {
+	Mapper     NetworkMapper
+	NetLoc     NetworkLocator
+	Repository LocationRepository
+}
+
+// LocateIP takes an IPv4 address and attempts geolocation, providing an
+// approximate geographic location associated with the IP address.
+func (l Geolocator) LocateIP(addr net.IP) (Location, error) {
+	var loc Location
+	network, err := l.Mapper.FindNetwork(addr)
+	if err != nil {
+		return loc, fmt.Errorf("unknown network for: %s", addr)
+	}
+	locationID, err := l.NetLoc.Map(network)
+	if err != nil {
+		return loc, fmt.Errorf("unknown location for: %s", network)
+	}
+	location, err := l.Repository.FindByID(locationID)
+	if err != nil {
+		return loc, fmt.Errorf("location unavailable: %s", locationID)
+	}
+	return location, nil
 }
